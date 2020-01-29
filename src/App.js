9b6ea34e7087b758837358db4bea7c21216aa09a
Samuel Tijani <omalitijani@gmail.com>
2020-01-29 18:21:46 +0100

    Check todo items as complete or uncomplete

--- src/App.js
+++ src/App.js
@@ -13,20 +13,30 @@ class App extends Component {
       {
         id:2,
         title: 'Dinner with wife',
-        completed: true
+        completed: false
       },
       {
-        id:1,
+        id:3,
         title: 'Meeting with boss',
         completed: false
       }
     ]
   }
 
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })
+  });
+}
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -14,16 +14,13 @@ class TodoItem extends Component {
 
     }
 
-    markComplete(e) {
-        console.log(this.props)
-    }
-
     render() {
+        const {id, title} = this.props.todo;
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.markComplete}/> {' '}
-                    {this.props.todo.title}
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    {title}
                 </p>
             </div>
         )
